d91e980deb85c7ed22a76ddf945daee2e70963b7
nattiya <nattiya@gmail.com>
2021-04-20 19:28:14 +0700

    Exported all questions to a pickle file

--- topic_classification/history_parser.py
+++ topic_classification/history_parser.py
@@ -4,20 +4,95 @@ import json
 import os
 import time
 
+import sys
+sys.path.insert(0, "../BERT-FAQ")
+print(sys.path)
+from shared.utils import dump_to_pickle
+
 class History_Parser(object):
     """ Class for parsing & extracting CovidFAQ data from .jsonl files """
     
-    def __init__(self, path="./data/", snapshots = ["schema_v0.1", "schema_v0.2", "schema_v0.3"]):
+    def __init__(self, path="./topic_classification/data/", snapshots = ["schema_v0.1", "schema_v0.2", "schema_v0.3"]):
         self.path = path
         self.snapshots = snapshots
         self.all_data = []
         self.train_data = []
         self.infer_data = []
 
+        self.list_of_questions = []
+
+    def get_all_questions(self, output_path):
+        i = 0
+
+        # Load data from different snapshots (i.e., stored in schema_v0.x folders)
+        for snapshot in self.snapshots:
+            input_path = self.path + snapshot + "/"
+            for filename in os.listdir(input_path):
+                if filename.endswith(".jsonl"): 
+                    with open(input_path + filename, 'r') as json_file:
+                        json_list = list(json_file)
+
+                        for json_str in json_list:
+                            result = json.loads(json_str)
+                            
+                            topics = []
+                            if 'topic' in result:
+                                if isinstance(result['topic'], list):
+                                    topics = result['topic']
+                                elif isinstance(result['topic'], str) and (len(result['topic']) > 0):
+                                    l = [result['topic']]
+                                    topics = l
+
+                            sourceName = ""
+                            if 'sourceName' in result:
+                                sourceName = result['sourceName']
+                            
+                            sourceUrl = ""
+                            if 'sourceUrl' in result:
+                                sourceUrl = result['sourceUrl']
+
+                            time = None
+                            date = None
+                            if 'dateLastChanged' in result:
+                                time = result['dateLastChanged']
+                                date = str(dt.datetime.utcfromtimestamp(time).strftime("%Y%m%d"))
+                                month = str(dt.datetime.utcfromtimestamp(time).strftime("%Y-%m"))
+                            elif 'dateScraped' in result:
+                                time = result['dateScraped']
+                                date = str(dt.datetime.utcfromtimestamp(time).strftime("%Y%m%d"))
+                                month = str(dt.datetime.utcfromtimestamp(time).strftime("%Y-%m"))
+
+                            wayBackUrl = ""
+                            if date is not None and sourceUrl is not None:
+                                wayBackUrl = "https://web.archive.org/web/" + date + "/" + sourceUrl
+
+                            question = ""
+                            if 'questionText' in result:
+                                question = result['questionText']
+                            elif 'question' in result:
+                                question = result['question']
+
+                            answer = ""
+                            if 'answerText' in result:
+                                answer = result['answerText']
+                            elif 'answer' in result:
+                                answer = result['answer']
+                            
+                            version = snapshot
+
+                            self.list_of_questions.append(question)
+                            i = i+1
+                else:
+                    # Skip parsing non json files
+                    continue
+            
+            print(i)
+            return self.list_of_questions
+
     def generate_train_test(self, output_path):
 
         # Read FAQ data with manually annotated topics
-        filename = '../data/CovidFAQ/historical_faqs_for_indexing.tsv'
+        filename = './data/CovidFAQ/historical_faqs_for_indexing.tsv'
         hist_data = pd.read_csv(filename, sep='\t', header=0)
         hist_data.drop(['wayBackUrl', 'dateScraped', 'date', 'dateStr', 'month'], axis=1, inplace=True)
         print(len(hist_data))
@@ -152,5 +227,9 @@ class History_Parser(object):
 if __name__ == "__main__":
     h = History_Parser()
 
-    output_path = "./data/"
-    h.generate_train_test(output_path)
+    output_path = "./topic_classification/data/"
+    #h.generate_train_test(output_path)
+    
+    questions = h.get_all_questions(output_path)
+    print(len(questions))
+    dump_to_pickle(questions, "./topic_classification/data/questions.pkl")
